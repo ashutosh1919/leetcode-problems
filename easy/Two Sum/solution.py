68dd8ebc8476cdcdf3016a95acafe6c66a8e46d2
Ashutosh Hathidara <ashutosh1919@Ashutoshs-MacBook-Pro.local>
2021-08-21 15:27:00 -0400

    Two Sum

---
+++ easy/Two Sum/solution.py
@@ -0,0 +1,13 @@
+# O(nlogn) solution
+# Approach: iterate through the list keeping the track of available elements and their indices in dict. When the complementry item arrives, return the indices.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+        for i in range(len(nums)):
+            b = target - nums[i]
+            if b in mp:
+                return [i, mp[b]]
+            mp[nums[i]] = i
+                
+        return []
